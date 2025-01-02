fc4cbdb92cfbd28653be110eeec8b13b55645949
Ivanarre <arre2397@gmail.com>
2025-01-02 22:32:58 +0800

    logical operator

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ const buttonValues = [
 const rightSymbols = ["÷", "×", "-", "+", "="];
 const topSymbols = ["AC", "+/-", "%"];
 
+let A = 0;
+let operator = null;
+let B = null;
+
+function clearAll() {
+  A = 0;
+  operator = null;
+  B = null;
+}
+
+const display = document.getElementById("display");
+
 for (let i = 0; i < buttonValues.length; i++) {
   let value = buttonValues[i];
   let button = document.createElement("button");
@@ -25,6 +37,69 @@ for (let i = 0; i < buttonValues.length; i++) {
     button.style.backgroundColor = "#D4D4D2";
     button.style.color = "#1C1C1C";
   }
+
+  button.addEventListener("click", function() {
+    if (rightSymbols.includes(value)) {
+      if (value == "=") {
+        if (A != null) {
+          B = display.value;
+          let numA = Number(A);
+          let numB = Number(B);
+
+          if (operator == "+") {
+            display.value = numA/numB;
+          }
+          else if (operator == "×") {
+            display.value = numA*numB;
+          }
+          else if (operator == "-") {
+            display.value = numA-numB;
+          }
+          else if (operator == "+") {
+            display.value = numA+numB;
+          }
+          clearAll();
+        }
+      }
+      else {
+        operator = value;
+        A = display.value;
+        display.value = "";
+      }
+    }
+    else if (topSymbols.includes(value)) {
+      if (value == "AC") {
+        clearAll();
+        display.value = "";
+      }
+      else if (value == "+/-") {
+        if (display.value != "" && display.value != "0") {
+          if (display.value[0] == "-") {
+            display.value = display.value.slice(1);
+          }
+          else {
+            display.value = "-" + display.value;
+          }
+        }
+      }
+      else if (value == "%") {
+        display.value = Number(display.value) / 100;
+      }
+    }
+    else {
+      if (value == ".") {
+        if (display.value != "" && !display.value.includes(value)) {
+          display.value += value;
+        }
+      }
+      else if (display.value == "0") {
+        display.value = value;
+      }
+      else {
+        display.value += value;
+      }
+    }
+  })
 
   document.getElementById("buttons").appendChild(button);
 }
