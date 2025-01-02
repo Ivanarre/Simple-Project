502347c77d9c87455d63e876bd4de50020358096
Ivanarre <arre2397@gmail.com>
2025-01-02 21:53:57 +0800

    calculator Improvements

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ const buttonValues = [
 ];
 const rightSymbols = ["÷", "×", "-", "+", "="];
 const topSymbols = ["AC", "+/-", "%"];
+
+for (let i = 0; i < buttonValues.length; i++) {
+  let value = buttonValues[i];
+  let button = document.createElement("button");
+  button.innerText = value;
+
+  // styling button colors
+  if (value == "0") {
+    button.style.width = "200px";
+    button.style.gridColumn = "span 2";
+  }
+  if (rightSymbols.includes(value)) {
+    button.style.backgroundColor = "#FF9500";
+  }
+  else if (topSymbols.includes(value)) {
+    button.style.backgroundColor = "#D4D4D2";
+    button.style.color = "#1C1C1C";
+  }
+
+  document.getElementById("buttons").appendChild(button);
+}
